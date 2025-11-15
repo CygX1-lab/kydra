66c6cc20701c158073ce58b8c29707d1642101ac
CygX1-lab <cygnx1@gmail.com>
2025-11-15 20:38:10 +0100

    Delete build-test directory

--- build-test/src/icons/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aardvark/git/kydra/src/icons
-# Build directory: /home/aardvark/git/kydra/build-test/src/icons
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
